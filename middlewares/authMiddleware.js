ed325198df10987faca7b39cc194c28e80ab060d
2kwattz <prakashbhatia1970@gmail.com>
2026-05-23 14:59:57 +0530

    Authentication Middleware Created

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,11 +1,58 @@
-const jwt = require("jsonwebtoken"); 
+const jwt = require("jsonwebtoken");
 
-const authMiddleware = async (req,res,next) => {
-    try{
+const authMiddleware = async (req, res, next) => {
+    try {
         console.log("[*] Authenticating User ")
+        const authHeader = req.headers.authorization; // Fetching JWT Bearer Token
+
+        // If Authorization Header is missing
+        if (!authHeader) {
+
+            console.error("[*] Auth Header Missing")
+            return res.status(401).json({
+                status: false,
+                message:"Authorization header missing"
+            });
+        }
+
+        // If Authorization Header has invalid format
+        if (!authHeader.startsWith("Bearer ")) {
+
+            return res.status(401).json({
+                status: false,
+                message:"Invalid Authorization Format"
+            })};
+
+        // Fetching JWT Token
+        const token = authHeader.split(" ")[1];
+
+        // If JWT Token is missing
+        if (!token) {
+            return res.status(401).json({
+                status: false,
+                message: "Token missing"
+            });
+
+        }
+
+        // Verifying JWT Token Signature
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET_KEY
+        );
+
+        // Appending user details in Request object
+        req.user = decoded;
+        next();
+
+
     }
-    catch(error){
+    catch (error) {
         console.log(`[*] Error in Auth Middleware ${error.message || error} `);
+        return res.status(401).json({
+            status: false,
+            message:"Invalid or Expired Token"
+        })
     }
 }
 
