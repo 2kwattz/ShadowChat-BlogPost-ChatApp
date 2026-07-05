15aaf02c69a5c1d66bdfd70c968ede4f227660ca
Roshan Bhatia <prakashbhatia1970@gmail.com>
2026-07-05 22:03:36 +0530

    WIP: CreateCommunity changes

--- client/src/pages/CreateCommunity/CreateCommunity.jsx
+++ client/src/pages/CreateCommunity/CreateCommunity.jsx
@@ -17,6 +17,12 @@ const CreateCommunity = () => {
                     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><path d="M19 12H5M12 5l-7 7 7 7"></path></svg>
                     <p>Back to communities</p>
                 </div>
+
+                <div>
+                    <h1 style={{color:"white"}}>
+                    Create your Community
+                    </h1>
+                    </div>
             </section>
             <section className='two-col-container'>
                 <div className='two-col-left'>
